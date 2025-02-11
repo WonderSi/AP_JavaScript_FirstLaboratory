7c9af49239a547fe54d4017bab2d000e04b7cfbd
WonderSi <kirillkozlov00@gmail.com>
2025-02-11 23:56:25 +0700

    update flow control 5

--- FlowControl/5/index.js
+++ FlowControl/5/index.js
@@ -1,10 +1,10 @@
-let result = [];
+let result = 0;
 
 for (let i = 1; i <= 15; i++) {
     if (i == 5 || i == 7) {
         continue;
     }
-    result.add(i)
+    result += i;
 }
 
 console.log(result)
