269ccc8b4095f57eaf4c38c0e72b929f1b68f3f9
WonderSi <kirillkozlov00@gmail.com>
2025-02-11 23:52:01 +0700

    update flow control 4

--- FlowControl/4/index.js
+++ FlowControl/4/index.js
@@ -1,11 +1,15 @@
 let number = 0;
 while(number <= 50) {
-    console.log(number);
+    if (number % 2 != 0) {
+        console.log(number);
+    }
     number++;
 }
 
 console.log("-------")
 
 for (let i = 0; i <= 50; i++) {
-    console.log(i);
+    if (i % 2 != 0) {
+        console.log(i);
+    }
 }
